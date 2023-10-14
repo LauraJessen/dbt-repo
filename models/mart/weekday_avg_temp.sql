0d887644cdd0fc3382cd7f523a11a3da26cc870e
LauraJessen <laura_jessen@web.de>
2023-10-14 10:32:51 +0200

    mart

--- models/mart/weekday_avg_temp.sql
+++ models/mart/weekday_avg_temp.sql
@@ -6,6 +6,10 @@ weekly_avg_temp AS (
      SELECT date_part('week', date) as week_num,
      avg(avgtemp) as avg_temp
      FROM prep_temp_data
-     group by(week_num)
+     group by(week_num, date)
 )
 SELECT * FROM weekly_avg_temp
+
+
+
+--column "prep_temp_data.date" must appear in the GROUP BY clause or be used in an aggregate function
